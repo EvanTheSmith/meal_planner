f56c6b7a5c1404508eba5231dc5d074005a0d581
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-11-18 05:38:38 -0600

    Populated some items in database

--- backend/app/controllers/meals_controller.rb
+++ backend/app/controllers/meals_controller.rb
@@ -1,7 +1,6 @@
 class MealsController < ApplicationController
     def index
       meals = Meal.all
-      # render json: meals, include: [:items]
       render json: MealSerializer.new(meals).to_serialized_json
     end
 end
