18b9af35be4aec7c9756a4d4578b109457e2c567
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-11-18 05:22:51 -0600

    Created meal serializer / refactored meals controller

--- backend/app/controllers/meals_controller.rb
+++ backend/app/controllers/meals_controller.rb
@@ -1,6 +1,7 @@
 class MealsController < ApplicationController
     def index
       meals = Meal.all
-      render json: meals, include: [:items]
+      # render json: meals, include: [:items]
+      render json: MealSerializer.new(meals).to_serialized_json
     end
 end
---
+++ backend/app/services/meal_serializer.rb
@@ -0,0 +1,11 @@
+class MealSerializer
+  def initialize(meal_object)
+    @meal = meal_object
+  end
+
+  def to_serialized_json
+    @meal.to_json(:include => {:items => {:only => [:name, :kind, :calories]}}, 
+    :except => [:updated_at, :created_at])
+  end
+    
+end
