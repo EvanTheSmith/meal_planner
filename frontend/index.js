d742b08243e6da4bb409c1e2f52b5e25845cf37e
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-11-25 17:11:20 -0600

    Created one fetch request + started building the function to display the data in the browser dynamically

--- frontend/index.js
+++ frontend/index.js
@@ -3,4 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
 let theMeals = document.getElementById("meals_go_here");
 let theItems = document.getElementById("items_go_here");
 
+fetch('http://localhost:3000/meals')
+.then(response => response.json())
+.then(data => renderMeals(data));
+
+function renderMeals(theMeals) {
+    // This function will render the meals and their respective items into the table rows
+}
+
 })
