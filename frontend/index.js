639752cdb4982f146c725d979a93c6e9f25bd3ae
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-11-25 18:45:20 -0600

    All items are rendering to the page via Javascript

--- frontend/index.js
+++ frontend/index.js
@@ -8,13 +8,25 @@ fetch('http://localhost:3000/meals')
 .then(response => response.json())
 .then(data => renderMeals(data));
 
-// This function will render the meals and their respective items into the table rows
+// This function will render Meals and their respective Items
 function renderMeals(theMeals) {
+    // Render Meals first
     for (meal of theMeals) {
-        let mealColumn = document.createElement("th"); // create Header Column for a Meal
-        mealColumn.innerText = meal.name; // Name meal
-        mealsRow.appendChild(mealColumn); // Attach meal to Meals Row
+        let mealTH = document.createElement("th"); // create TH for this Meal
+        mealTH.innerText = meal.name; // Name the meal
+        mealsRow.appendChild(mealTH); // Attach the meal's TH to Meals Row
 
+        let itemsTD = document.createElement("td"); // create TD for all this Meal's items
+        itemsRow.appendChild(itemsTD); // Append this TD to the Items Row
+        let itemsTable = document.createElement("table"); // create a table for all this Meal's items
+        
+        // Then Render those items
+        for (item of meal.items) {
+            let itemName = document.createElement("tr");
+            itemName.innerText = item.name; // Name item
+            itemsTable.appendChild(itemName);
+            itemsTD.appendChild(itemsTable);
+        }
     }
 }
 
