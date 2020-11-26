1c2da9af8291182a6596912eea348280ed93d04c
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-11-25 18:55:13 -0600

    Updated styling, refactored variable names

--- frontend/index.js
+++ frontend/index.js
@@ -6,26 +6,23 @@ let itemsRow = document.getElementById("items_go_here"); // Items Row
 // Initial fetch request needed to get data for rendering the page
 fetch('http://localhost:3000/meals')
 .then(response => response.json())
-.then(data => renderMeals(data));
+.then(data => renderMealsAndItems(data));
 
 // This function will render Meals and their respective Items
-function renderMeals(theMeals) {
-    // Render Meals first
-    for (meal of theMeals) {
-        let mealTH = document.createElement("th"); // create TH for this Meal
-        mealTH.innerText = meal.name; // Name the meal
-        mealsRow.appendChild(mealTH); // Attach the meal's TH to Meals Row
+function renderMealsAndItems(theMeals) {
+    for (meal of theMeals) { // Render Meals
+        let meal_TH = document.createElement("th"); // create TH for this Meal
+        meal_TH.innerText = meal.name;
+        mealsRow.appendChild(meal_TH); // Attach the meal's TH to Meals Row
 
-        let itemsTD = document.createElement("td"); // create TD for all this Meal's items
-        itemsRow.appendChild(itemsTD); // Append this TD to the Items Row
+        let items_TD = document.createElement("td"); // create TD for all this Meal's items
+        itemsRow.appendChild(items_TD); // Append this TD to the Items Row
         let itemsTable = document.createElement("table"); // create a table for all this Meal's items
         
-        // Then Render those items
-        for (item of meal.items) {
-            let itemName = document.createElement("tr");
-            itemName.innerText = item.name; // Name item
+        for (item of meal.items) { // Render Items for this Meal
+            let itemName = document.createElement("tr"); itemName.innerText = item.name;
             itemsTable.appendChild(itemName);
-            itemsTD.appendChild(itemsTable);
+            items_TD.appendChild(itemsTable);
         }
     }
 }
