2cd0357c2304e7f5fd6f423c3a3534491484bc5a
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-11-25 19:34:56 -0600

    Added calorie counting function + some refactoring

--- frontend/index.js
+++ frontend/index.js
@@ -11,8 +11,10 @@ fetch('http://localhost:3000/meals')
 // This function will render Meals and their respective Items
 function renderMealsAndItems(theMeals) {
     for (meal of theMeals) { // Render Meals
-        let meal_TH = document.createElement("th"); meal_TH.innerText = meal.name;
-        mealsRow.appendChild(meal_TH); // Attach this meal to the Meals Row
+        let meal_TH = document.createElement("th");
+        let mealCalories = countCalories(meal.items);
+        meal_TH.innerText = meal.name + " - "+mealCalories+" calories";
+        mealsRow.appendChild(meal_TH);
 
         let items_TD = document.createElement("td"); itemsRow.appendChild(items_TD);
         let itemsTable = document.createElement("table"); // table for all this Meal's items
@@ -21,12 +23,19 @@ function renderMealsAndItems(theMeals) {
             let itemAttr = document.createElement("tr"); // a row for all this item's attributes
             let itemName = document.createElement("td"); itemName.innerText = item.name;
             let itemKind = document.createElement("td"); itemKind.innerText = item.kind;
-            let itemCalories = document.createElement("td"); itemCalories.innerText = item.calories;
+            let itemCalories = document.createElement("td"); itemCalories.innerText = item.calories+" calories";
             itemAttr.appendChild(itemName); itemAttr.appendChild(itemKind); itemAttr.appendChild(itemCalories);
             itemsTable.appendChild(itemAttr); // < ^ these two rows attach all the attributes
         }
         items_TD.appendChild(itemsTable);
     }
+}
+
+function countCalories(mealItems) {
+    let calorieArray = [];
+    let reduceAlgo = (accumulator, currentValue) => accumulator + currentValue;
+    for (item of mealItems) { calorieArray.push(item.calories); }
+    return calorieArray.reduce(reduceAlgo, 0);
 }
 
 })
