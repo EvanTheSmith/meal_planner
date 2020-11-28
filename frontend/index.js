0623d66d1de04adee277e61124d9b5d1d6ad78e4
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-11-28 12:58:05 -0600

    Refactored meals to a class, contained function within class

--- frontend/index.js
+++ frontend/index.js
@@ -4,6 +4,21 @@ const MEALS_URL = `${HOST_URL}/meals/`
 const MEALS_ROW = document.getElementById("meals_go_here");
 const ITEMS_ROW = document.getElementById("items_go_here");
 
+// The Class
+class Meal {
+    constructor(name, items) {
+      this.name = name;
+      this.items = items;
+    }
+
+    countCalories() {
+     let calorieArray = [];
+     let reduceAlgo = (a, b) => a + b;
+     for (let one of this.items) { calorieArray.push(one.calories); }
+     return calorieArray.reduce(reduceAlgo, 0);
+    }
+}
+
 // This starts filling the page when the page is fully loaded
 document.addEventListener('DOMContentLoaded', () => { fetchFood(); });
 
@@ -11,21 +26,21 @@ document.addEventListener('DOMContentLoaded', () => { fetchFood(); });
 function fetchFood() {
 fetch(MEALS_URL)
 .then(response => response.json())
-.then(data => renderMealsAndItems(data));
+.then(data => renderMealsAndItems(data)); // Feed that data into my rendering function
 }
 
 // This function will render Meals and their respective Items
 function renderMealsAndItems(theMeals) {
     for (meal of theMeals) { // Render Meals
+        let new_meal = new Meal(meal.name, meal.items);
         let meal_TH = document.createElement("th");
-        let mealCalories = countCalories(meal);
-        meal_TH.innerText = meal.name + " - "+mealCalories+" calories";
+        meal_TH.innerText = new_meal.name + " - "+new_meal.countCalories()+" calories";
         MEALS_ROW.appendChild(meal_TH);
 
         let items_TD = document.createElement("td"); ITEMS_ROW.appendChild(items_TD);
         let itemsTable = document.createElement("table"); // table for all this Meal's items
         
-        for (item of meal.items) { // Render Items for this Meal to the new table
+        for (item of new_meal.items) { // Render Items for this Meal to the new table
             let itemAttr = document.createElement("tr"); // a row for all this item's attributes
             let itemName = document.createElement("td"); itemName.innerText = item.name;
             let itemKind = document.createElement("td"); itemKind.innerText = item.kind;
