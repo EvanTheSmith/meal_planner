abcc0b02ef5b922b58683383ec0d28ce3b79e152
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-03-15 20:17:32 -0500

    deciding between use of editButton function or createItem function for style change

--- frontend/index.js
+++ frontend/index.js
@@ -106,7 +106,7 @@ function createItem(item, itemsTable) { // this creates the node for an item and
     
     // edit/delete buttons
     let ediButton = document.createElement("div"); ediButton.id = "edit"; ediButton.innerText = "edit ";
-    ediButton.addEventListener("click", function() {editButton(itemNode, item, edibutton);});
+    ediButton.addEventListener("click", function() {editButton(itemNode, item); ediButton.classList.add("no_style");});
     let delButton = document.createElement("div"); delButton.id = "delete"; delButton.innerText = "delete";
     delButton.addEventListener("click", function() {deleteItem(itemNode, item)});
 
